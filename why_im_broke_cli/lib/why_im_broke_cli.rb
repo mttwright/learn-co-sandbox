ebdb2e7933c326226ea5814a5adad4d0d536de63
Matt Wright <mttwright@gmail.com>
2021-05-28 13:01:15 +0000

    Automatically backed up by Learn

--- why_im_broke_cli/lib/why_im_broke_cli.rb
+++
@@ -1,8 +0,0 @@
-require "nokogiri"
-require "pry"
-require "open-uri"
-
-require_relative "./why_im_broke_cli/version"
-require_relative "./why_im_broke_cli/cli"
-require_relative "./why_im_broke_cli/item"
-require_relative "./why_im_broke_cli/scraper"
